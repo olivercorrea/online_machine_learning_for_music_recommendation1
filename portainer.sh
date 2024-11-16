18c36ee1a6d2c4fc8f952bdd13ee88c7b32534bd
olivercorrea <olivercorreacabana@outlook.es>
2024-11-15 21:49:03 -0500

    t7: portainer fixed

--- portainer.sh
+++ portainer.sh
@@ -4,19 +4,9 @@ echo "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀�
 echo "🚀 Deploying Portainer"
 
 # Crear un archivo de swap de 1 GB
-sudo fallocate -l 1G /swapfile
-
-# Cambiar los permisos para que solo root pueda leer y escribir
-sudo chmod 600 /swapfile
-
-# Configurar el archivo como swap
-sudo mkswap /swapfile
-
-# Activar el swap
-sudo swapon /swapfile
-
-# Verificar que el swap esté activo
-sudo swapon --show
+sudo docker run -d -p 9000:9000 --restart always --name portainer \
+-v /var/run/docker.sock:/var/run/docker.sock \
+portainer/portainer-ce
 
 PUBLIC_IP=$(curl -s ifconfig.me)
 echo "IP pública: http://$PUBLIC_IP:9000/"
