6808c8f67e5d536ef4f0bd06eebe857c3eb27f13
olivercorrea <olivercorreacabana@outlook.es>
2024-11-21 08:50:36 -0500

    tags changed

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ NC='\033[0m' # Sin color
 
 # Definir la variable dockerId y version
 dockerId="rimaro"
-version="v5"
+version="v6"
 
 echo -e "${CYAN}☄️🪁------------------------------------------------------------🪁☄️${NC}"
 echo -e "${GREEN}🚀 Building...${NC}"
